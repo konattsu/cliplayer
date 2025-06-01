204e2bc8968e98bc57efebde22495c401260e04b
konattsu <kokonattsu1112@gmail.com>
2025-06-01 11:15:21 +0000

    vitest出来るように

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react-swc";
+import eslint from "vite-plugin-eslint";
+import { defineConfig } from "vitest/config";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslint({
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      emitWarning: true,
+      emitError: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    target: "esnext",
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    globals: true,
+  },
 });
