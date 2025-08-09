b816f41e0a8c02b07c612ad3534d9c10029571a1
konattsu <139730998+konattsu@users.noreply.github.com>
2025-08-09 15:03:23 +0000

    コマンド使いやすく

--- music_data/src/cli.rs
+++ music_data/src/cli.rs
@@ -1,17 +1,13 @@
 mod file_paths;
-mod min;
-mod min_clips;
 mod music_root;
 mod parser;
 mod tracing_level;
 mod video_ids;
 
 pub use file_paths::FilePathsFromCli;
-pub use min::OutputMinPathFromCli;
-pub use min_clips::OutputMinClipsPathFromCli;
 pub use music_root::MusicLibraryCli;
 pub use parser::{
-    ApplyCommands, ArtistCommands, Cli, Commands, TraceLevel, ValidateCommands,
+    ApplyCommands, Cli, Commands, TraceLevel, UtilCommands, ValidateCommands,
 };
 pub use tracing_level::TracingLevel;
 pub use video_ids::VideoIdsFromCli;
